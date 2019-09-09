e32ba419c93fcd951ff1c73f578c10c1d222a3c6
LauraAlbersmann <laura-albersmann@web.de>
2019-09-09 11:17:27 +0200

    feat(system): removed JavaScript Method

--- module_system/scripts/kajona/Forms.ts
+++ module_system/scripts/kajona/Forms.ts
@@ -424,6 +424,21 @@ class Forms {
             'json'
         )
     }
+
+    // Auto-Grow-TextArea script.
+    // Script copyright (C) 2011 www.cryer.co.uk.
+    // Script is free to use provided this copyright header is included.
+    public static AutoGrowTextArea (textField) {
+        if (textField.clientHeight < textField.scrollHeight)
+        {
+            textField.style.height = textField.scrollHeight + "px";
+            if (textField.clientHeight < textField.scrollHeight)
+            {
+                textField.style.height =
+                    (textField.scrollHeight * 2 - textField.clientHeight) + "px";
+            }
+        }
+    }
 }
 ;(<any>window).Forms = Forms
 export default Forms
