d6c095d12addaabcf4337f7730569721f0bfe2fc
Dhafer <soprano1993@live.fr>
2019-09-18 18:02:52 +0200

    fix(search) : updated SearchServices to use the new api

--- module_search/scripts/services/SearchServices.ts
+++ module_search/scripts/services/SearchServices.ts
@@ -6,35 +6,24 @@ import qs from 'qs'
  * makes all the necessary api calls of the searchModule
  */
 class SearchServices {
-    // todo add correct payload types after SearchApiController is ready
     public static async triggerSearch (payload) : Promise<any[]> {
         const [err, res] = await to(axios({
-            url: '/xml.php',
-            method: 'POST',
+            url: '/api.php/v1/search',
+            method: 'GET',
             params: {
-                module: 'search',
-                action: 'getFilteredSearch',
                 search_query: payload.searchQuery,
                 filtermodules: payload.selectedIds,
                 search_changestartdate: payload.startDate,
                 search_changeenddate: payload.endDate,
                 search_formfilteruser_id: payload.selectedUser
-            },
-            paramsSerializer: (params : any) => {
-                return qs.stringify(params, { arrayFormat: 'comma' })
             }
-
         }))
         return [err, res]
     }
     public static async getFilterModules () : Promise<any[]> {
         const [err, res] = await to(axios({
-            url: '/xml.php?',
-            method: 'GET',
-            params: {
-                module: 'search',
-                action: 'getModulesForFilter'
-            }
+            url: '/api.php/v1/search/modules',
+            method: 'GET'
         }))
         return [err, res]
     }
