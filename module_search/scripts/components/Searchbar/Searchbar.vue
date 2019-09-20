3db1befa8e54438d56cd73f6729d60b15a44b56c
Dhafer Harrathi <dhaferharrathi@artemeon.de>
2019-09-20 14:41:53 +0200

    feat(search) : detached the header input from Searchbar component

--- module_search/scripts/components/Searchbar/Searchbar.vue
+++ module_search/scripts/components/Searchbar/Searchbar.vue
@@ -1,41 +1,34 @@
 <template >
   <div v-if="langFetched" class="core-component-searchbar">
-    <form class="navbar-search pull-left " v-if="!dialogIsOpen">
-      <div class="input-group">
-        <input
-          class="form-control search-query"
-          @mousedown="open"
-          :placeholder="$t('dashboard.globalSearchPlaceholder')"
-        />
-        <span class="input-group-addon">
-          <i class="fa fa-search" aria-hidden="true"></i>
-        </span>
-      </div>
-    </form>
-    <Modal :show="dialogIsOpen" @close="close" @open="onModalOpen">
+    <Modal :show="dialogIsOpen" @close="close()" @open="onModalOpen">
       <div class="modal-body">
         <div class="row container-fluid">
-        <form @submit="onSubmit" class="navbar-search pull-left">
-          <div class="input-group">
-            <input
-              id="searchbarInput"
-              type="text"
-              name="search_query"
-              class="form-control search-query"
-              @input="onInput"
-              v-model="userInput"
-              @mousedown="open"
-              autocomplete="off"
-              :placeholder="$t('dashboard.globalSearchPlaceholder')"
-            />
-            <span class="input-group-addon searchbarFilterToggle" @click="toggleFilter" :title="$t('search.form_additionalheader')" rel="tooltip">
-              <i
-                class="fa fa-caret-down"
-                v-bind:class="{'fa fa-caret-up' : !filterIsOpen , 'fa fa-caret-up' : filterIsOpen}"
-              ></i>
-            </span>
-          </div>
-        </form>
+          <form @submit="onSubmit" class="navbar-search pull-left">
+            <div class="input-group">
+              <input
+                id="searchbarInput"
+                type="text"
+                name="search_query"
+                class="form-control search-query"
+                @input="onInput"
+                v-model="userInput"
+                @mousedown="open"
+                autocomplete="off"
+                :placeholder="$t('dashboard.globalSearchPlaceholder')"
+              />
+              <span
+                class="input-group-addon searchbarFilterToggle"
+                @click="toggleFilter"
+                :title="$t('search.form_additionalheader')"
+                rel="tooltip"
+              >
+                <i
+                  class="fa fa-caret-down"
+                  v-bind:class="{'fa fa-caret-up' : !filterIsOpen , 'fa fa-caret-up' : filterIsOpen}"
+                ></i>
+              </span>
+            </div>
+          </form>
         </div>
         <SearchbarFilter v-if="dialogIsOpen && filterIsOpen"></SearchbarFilter>
         <Loader :loading="isLoading"></Loader>
