f18e9a376e5b4937c09ac0cc01001a1b6429644a
Dhafer <soprano1993@live.fr>
2019-10-20 17:09:36 +0200

    fix(system) : catch error if occurs in HttpClient + added JSDocs

--- module_system/scripts/kajona/HttpClient.ts
+++ module_system/scripts/kajona/HttpClient.ts
@@ -1,47 +1,53 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
+import to from 'await-to-js'
 
-class HttpClient {
-    public static get() {}
-
-    public static post() {}
-
-    public static delete() {}
-
-    /**
- *
- * @param url
- * @param data
+/**
+ * AJAX functions for connecting to the server
  */
-    public static async asyncGet(url: string, params: any): Promise<any> {
-        const res = await axios({
+class HttpClient {
+    /**
+  * GET function : returns a promise containing the response object + error if occurs
+  * can be used as an async function with async/await or with .then
+  * @param url url of the ajax call
+  * @param params params is an object example {action: 'getUnreadMessagesCount' , module: 'messaging'  }. The params will be automatically serialized
+  */
+    public static async get(url: string, params: object): Promise<[ Error, AxiosResponse ]> {
+        const [error, response] = await to(axios({
             method: 'GET',
             url,
             params,
-        })
-        return res
+        }))
+        return [error, response]
     }
 
     /**
- *
- * @param url
- * @param params
- */
-    public static async asyncPost(url: string, params: any): Promise<any> {
-        const res = await axios({
+  * POST function : returns a promise containing the response object + error if occurs
+  * can be used as an async function with async/await or with .then
+  * @param url url of the ajax call
+  * @param params params is an object example {action: 'getUnreadMessagesCount' , module: 'messaging'  }. The params will be automatically serialized
+  */
+    public static async post(url: string, params: object): Promise<[Error, AxiosResponse ]> {
+        const [error, response] = await to(axios({
             method: 'POST',
             url,
             params,
-        })
-        return res
+        }))
+        return [error, response]
     }
 
-    public static async asyncDelete(url: string, params: any): Promise<any> {
-        const res = await axios({
+    /**
+  * DELETE function : returns a promise containing the response object + error if occurs
+  * can be used as an async function with async/await or with .then
+  * @param url url of the ajax call
+  * @param params params is an object example {action: 'getUnreadMessagesCount' , module: 'messaging'  }. The params will be automatically serialized
+  */
+    public static async delete(url: string, params: any): Promise<[Error, AxiosResponse ]> {
+        const [error, response] = await to(axios({
             method: 'DELETE',
             url,
             params,
-        })
-        return res
+        }))
+        return [error, response]
     }
 }
 (window as any).HttpClient = HttpClient
