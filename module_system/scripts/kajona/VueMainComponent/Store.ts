e0d097c0d0905eb65c4243dedc5631255ce801be
rym.rjab <rym.rjab@artemeon.de>
2019-10-28 11:18:15 +0100

    fix(system): removed module rating Detail from store

--- module_system/scripts/kajona/VueMainComponent/Store.ts
+++ module_system/scripts/kajona/VueMainComponent/Store.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import RatingDetailModule from 'core_customer/module_hsbcact/scripts/modules/RatingDetailModule'
 
 import SearchModule from 'core/module_search/scripts/modules/SearchModule'
 // import VueI18n from 'vue-i18n'
@@ -8,7 +7,6 @@ Vue.use(<any>Vuex)
 // Vue.use(VueI18n)
 export default new Vuex.Store({
     modules: {
-        SearchModule: SearchModule,
-        ratingModule: RatingDetailModule
-    }
+        SearchModule,
+    },
 })
