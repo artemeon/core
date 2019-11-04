570a00f6ea8129c86dfb6608b55cd93febd07085
rym.rjab <rym.rjab@artemeon.de>
2019-11-04 15:49:43 +0100

    fix(system): fixed pagination component und added dots between unshown page numbers

--- module_system/scripts/components/Pagination/Pagination.vue
+++ module_system/scripts/components/Pagination/Pagination.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="langFetched" class="pager core-component-pager">
-    <ul class="pagination">
+    <ul class="pagination" v-if="totalPages<10">
       <li v-if="current!==1" @click="previous()">
         <a>« {{$t('commons.commons_back')}}</a>
       </li>
@@ -12,6 +12,30 @@
       >
         <a v-bind:class="{active : current === index+1}">{{index+1}}</a>
       </li>
+      <li @click="next()" v-if="current!==totalPages">
+        <a>{{$t('commons.commons_continue')}} »</a>
+      </li>
+      <li @click="onTotalElementsClick">
+        <a>
+          {{$t("system.pageview_total")}}
+          {{totalElements}}
+        </a>
+      </li>
+    </ul>
+    <ul v-else class="pagination">
+      <li v-if="current!==1" @click="previous()">
+        <a>« {{$t('commons.commons_back')}}</a>
+      </li>
+      <li
+        v-for="page in items"
+        :key="page.label"
+        :class="{active : page.active === true}"
+        @click="changePage(page.label)"
+      >
+        <span v-if="page.disable">...</span>
+        <a :class="{active : page.active === true}" v-else>{{page.label}}</a>
+      </li>
+      <li></li>
       <li @click="next()" v-if="current!==totalPages">
         <a>{{$t('commons.commons_continue')}} »</a>
       </li>
