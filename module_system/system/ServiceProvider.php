0f49eb8efc9ecb0cc187c73e7e9283d052963478
Mike Marschall <mike.marschall@artemeon.de>
2019-11-04 11:32:59 +0100

    feat(system): add current system user provider abstraction

--- module_system/system/ServiceProvider.php
+++ module_system/system/ServiceProvider.php
@@ -377,5 +377,11 @@ class ServiceProvider implements ServiceProviderInterface
                 $c[self::STR_RIGHTS]
             );
         };
+
+        $objContainer[CurrentUserProviderInterface::class] = static function (Container $container): CurrentUserProviderInterface {
+            return new CurrentUserFromSessionProvider(
+                $container[self::STR_SESSION]
+            );
+        };
     }
 }
