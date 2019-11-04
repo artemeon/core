9f5642864728d481f74eda45650d33ea3e4e96d4
LauraAlbersmann <laura-albersmann@web.de>
2019-11-04 13:55:55 +0100

    fix(system): file Name

--- module_system/scripts/kajona/VueMainComponent/Store.ts
+++ module_system/scripts/kajona/VueMainComponent/Store.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import SearchModule from 'core/module_search/scripts/modules/SearchModule'
-import MenuModule from 'core/module_system/scripts/modules/MenuModule';
+import MenuModule from 'core/module_system/scripts/modules/MenuModule'
 // import VueI18n from 'vue-i18n'
 Vue.use(<any>Vuex)
 // Vue.use(VueI18n)
--- module_system/scripts/kajona/VueMainComponent/VueMain.ts
+++ module_system/scripts/kajona/VueMainComponent/VueMain.ts
@@ -1,6 +1,6 @@
 import { Vue, Component } from 'vue-property-decorator'
 import Searchbar from 'core/module_search/scripts/components/Searchbar/Searchbar.vue'
-import Menu from 'core/module_system/scripts/components/menu/Menu.vue'
+import Menu from 'core/module_system/scripts/components/Menu/Menu.vue'
 
 @Component({ components: { Searchbar, Menu } })
 class VueMain extends Vue {
