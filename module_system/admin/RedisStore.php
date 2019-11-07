ba9c57802657e79a60335d4e2fe51ffdff44c9da
Dhafer <soprano1993@live.fr>
2019-11-07 04:18:19 +0100

    feat(system) : added child Class "RedisStore"

---
+++ module_system/admin/RedisStore.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace Kajona\System\Admin;
+
+use Predis\Client as Redis;
+
+/**
+ *
+ * @author dhafer.harrathi@artemeon.de
+ * @since 7.2
+ */
+class RedisStore extends CacheStore
+{
+
+    public function __construct()
+    {
+        parent::__construct(new Redis());
+    }
+
+    /**
+     * returns value of the given key
+     * @param string $key
+     * @return string
+     */
+    public function get(string $key): string
+    {
+        $value = $this->store->get($key);
+        if ($value) {
+            return $value;
+        }
+        return "";
+
+    }
+
+    /**
+     * sets the key and value in redis cache
+     * @param string $key
+     * @param string $value
+     */
+    public function set(string $key, string $value): void
+    {
+        $this->store->set($key, $value);
+    }
+
+}
