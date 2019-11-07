688c3ad04ef7f0e6ee7e73e409ee2c618de0cbd4
Dhafer <soprano1993@live.fr>
2019-11-07 04:17:29 +0100

    feat(admin) : added parent class "CacheStore" for all types of cache stores to extend from

---
+++ module_system/admin/CacheStore.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace Kajona\System\Admin;
+
+use Predis\Client as Redis;
+
+/**
+ *
+ * @author dhafer.harrathi@artemeon.de
+ * @since 7.2
+ */
+class CacheStore
+{
+    /**
+     * @var Redis
+     */
+    protected $store;
+
+    /**
+     * CacheStore constructor.
+     * @param $store
+     */
+    public function __construct($store)
+    {
+        $this->store = $store;
+    }
+
+    /**
+     * @param string $key
+     */
+    public function get(string $key)
+    {
+
+    }
+
+    /**
+     * @param string $key
+     * @param string $value
+     */
+    public function set(string $key, string $value)
+    {
+
+    }
+}
+
