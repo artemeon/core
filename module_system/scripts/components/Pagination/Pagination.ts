82ae5766fecc5cc61363c0688c4fc3dc204dc168
Dhafer <soprano1993@live.fr>
2019-11-08 12:25:30 +0100

    feat(system) : added performance mode to Pagination component

--- module_system/scripts/components/Pagination/Pagination.ts
+++ module_system/scripts/components/Pagination/Pagination.ts
@@ -1,17 +1,30 @@
 import { Component, Mixins, Prop } from 'vue-property-decorator'
 import { LangMixin } from 'core/module_system/scripts/kajona/VueMixings'
+import uuid from 'uuid/v1'
+import Tooltip from 'core/module_system/scripts/kajona/Tooltip'
 
 @Component class Pagination extends Mixins(LangMixin(['commons', 'system'])) {
-@Prop({ type: Number, required: true }) totalPages !: number
+// for performance mode you need to pass lastPage as a property and ignore totalPages prop
+@Prop({ type: Number, required: false }) totalPages !: number
 
 @Prop({ type: Number, required: true }) currentPage !: number
 
-@Prop({ type: Number, required: true }) totalElements !: number
+@Prop({ type: [Number, String], required: true }) totalElements !: string | number
+
+@Prop({ type: Boolean, required: false }) lastPage !: boolean
+
+@Prop({ type: String, required: false }) tooltip !: string
+
 
 private current: number = null as number
 
+private totalEntriesNumberId: string = uuid()
+
 private mounted(): void {
     this.current = this.currentPage
+    if (this.tooltip) {
+        Tooltip.addTooltip(document.getElementById(this.totalEntriesNumberId), this.tooltip)
+    }
 }
 
 private changePage(page: number): void{
@@ -22,7 +35,12 @@ private changePage(page: number): void{
 }
 
 private next(): void {
-    if (this.current + 1 <= this.totalPages) {
+    if (this.totalPages) {
+        if (this.current + 1 <= this.totalPages) {
+            this.current = this.current + 1
+            this.$emit('change', this.current)
+        }
+    } else if (!this.lastPage) {
         this.current = this.current + 1
         this.$emit('change', this.current)
     }
@@ -39,7 +57,7 @@ private onTotalElementsClick(): void {
     this.$emit('totalElementsClick')
 }
 
-get items(): any[] {
+private get items(): any[] {
     const valPrev = this.current > 1 ? (this.current - 1) : 1
     const valNext = this.current < this.totalPages ? (this.current + 1) : this.totalPages
     const extraPrev = valPrev === 3 ? 2 : null
