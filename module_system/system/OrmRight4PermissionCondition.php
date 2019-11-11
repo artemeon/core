11b23ee4bf880fef4dfead6deae3b0489a903f80
LauraAlbersmann <laura-albersmann@web.de>
2019-11-11 09:16:57 +0100

    refactor(system): deleted fallback alternative block

---
+++ module_system/system/OrmRight4PermissionCondition.php
@@ -0,0 +1,67 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2007-2016 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
+********************************************************************************************************/
+
+namespace Kajona\System\System;
+
+
+/**
+ * A orm condition to filter by right4 permission for the currently logged in user
+ *
+ *
+ * @package Kajona\System\System
+ * @author laura.albersmann@artemeon.de
+ * @since 7.2
+ */
+class OrmRight4PermissionCondition extends OrmCondition
+{
+    private $strColumn = null;
+
+    /**
+     * @var OrmPermissionCondition
+     */
+    private $fallback;
+
+    /**
+     * OrmPermissionCondition constructor.
+     *
+     * @param string $column the column to query against
+     * @throws Exception
+     */
+    public function __construct($column = "agp_system.system_id")
+    {
+        parent::__construct("", array());
+        $this->strColumn = $column;
+
+        //fall back to the simple like logic for small amount of data
+        $this->fallback = new OrmPermissionCondition(Rights::$STR_RIGHT_RIGHT4, null, StringUtil::replace("system_id", "right_right4", $column));
+
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function getStrWhere()
+    {
+        if ($this->fallback !== null) {
+            return $this->fallback->getStrWhere();
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getArrParams()
+    {
+        if ($this->fallback !== null) {
+            return $this->fallback->getArrParams();
+        }
+        return [Session::getInstance()->getUserID()];
+    }
+
+
+
+}
