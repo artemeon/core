ae37ca558135a53742b1a3dd6dc8675c880a0af8
Stefan Idler <sidler@mulchprod.de>
2019-11-12 09:45:22 +0100

    chore(): removed hardcoded routes

--- module_system/scripts/kajona/VueMainComponent/VueRouter.ts
+++ module_system/scripts/kajona/VueMainComponent/VueRouter.ts
@@ -1,30 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import RatingDetail from 'core_agp/module_hsbcact/scripts/components/RatingDetail/RatingDetail.vue'
-import Reportgenerator from 'core_agp/module_reportconfigurator/scripts/components/Reportgenerator/Reportgenerator.vue'
+// import RatingDetail from 'core_agp/module_hsbcact/scripts/components/RatingDetail/RatingDetail.vue'
+// import Reportgenerator from 'core_agp/module_reportconfigurator/scripts/components/Reportgenerator/Reportgenerator.vue'
 import Router from '../Router'
 
 Vue.use(<any>VueRouter)
 
 const router = new VueRouter({
     routes: [
-        {
-            path: '/vm/reportconfigurator/:reportId/:page?',
-            name: 'reportconfigurator',
-            component: Reportgenerator,
-            beforeEnter: resetContainer,
-        },
-        {
-            path: '/vm/hsbcact/rating/:systemId',
-            component: RatingDetail,
-            beforeEnter: resetContainer,
-            props: (route) => ({
-                query: {
-                    startDate: route.query.startDate,
-                    endDate: route.query.endDate,
-                },
-            }),
-        },
+        // {
+        //     path: '/vm/reportconfigurator/:reportId/:page?',
+        //     name: 'reportconfigurator',
+        //     component: Reportgenerator,
+        //     beforeEnter: resetContainer,
+        // },
+        // {
+        //     path: '/vm/hsbcact/rating/:systemId',
+        //     component: RatingDetail,
+        //     beforeEnter: resetContainer,
+        //     props: (route) => ({
+        //         query: {
+        //             startDate: route.query.startDate,
+        //             endDate: route.query.endDate,
+        //         },
+        //     }),
+        // },
     ],
 })
 function resetContainer(to, from, next): void {
