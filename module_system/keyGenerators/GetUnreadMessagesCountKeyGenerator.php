9495663042b05ca0911b4f5aedb0a85b07830f40
Dhafer <soprano1993@live.fr>
2019-11-16 11:50:49 +0100

    feat(system) : added KeyGenerator

--- module_system/keyGenerators/GetUnreadMessagesCountKeyGenerator.php
+++ module_system/keyGenerators/GetUnreadMessagesCountKeyGenerator.php
@@ -1,22 +1,24 @@
 <?php
 
+
 namespace Kajona\System\KeyGenerator;
 
 
+use Kajona\System\Admin\KeyGeneratorInterface;
 use Slim\Http\Request as SlimRequest;
 
 /**
- * Class to manage all cache operations
+ * Key generator for /v1/messages/count end-point
  *
  * @author dhafer.harrathi@artemeon.de
  * @since 7.2
  */
-class MessagingKeyGenerator
+class GetUnreadMessagesCountKeyGenerator implements KeyGeneratorInterface
 {
-    public function getUnreadMessagesCount(SlimRequest $request): string
+
+    public function getKey(SlimRequest $request): string
     {
         //todo choose a better key pattern
         return '/' . $request->getUri()->getPath();
     }
-
 }
